5f19ce4d96da5274c81b182b7a4f5e0687724454
akjoshi2 <akjoshi2003@gmail.com>
2021-10-23 17:28:16 -0500

    Form

--- frontend/src/components/VolunteerEvents/VolunteerEvents.js
+++ frontend/src/components/VolunteerEvents/VolunteerEvents.js
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import { FormControl, FormLabel, Box, Heading, Button, Input, InputRightAddon, InputGroup, Textarea } from "@chakra-ui/react";
 import events from "../Calendar/events";
 
 function Events() {
+	const [name, setName] = useState();
+	const [email, setEmail] = useState();
+	const [desc, setDesc] = useState();
+	const [address, setAddress] = useState();
+	const [zip, setZip] = useState();
+
+	const createEvent = useCallback(() => {
+
+	},[])
 	return (
 		<div className="events">
 			<Box textAlign="center">
@@ -13,23 +22,40 @@ function Events() {
 				<FormLabel>
 					Organization Name
 				</FormLabel>
-				<Input />
+				<Input onChange={event => setName(event.currentTarget.value)} />
 			</FormControl>
 			<FormControl my={5} id="org-email" isRequired>
 				<FormLabel>
-					Email Address
+					Email
 				</FormLabel>
 				<InputGroup>
-					<Input type="email" />
-					<InputRightAddon children="@gmail.com" />
+					<Input type="email" placeholder="example@gmail.com" onChange={event => setEmail(event.currentTarget.value)} />
 				</InputGroup>
+			</FormControl>
+			<FormControl id="address" isRequired>
+				<FormLabel>
+					Address
+				</FormLabel>
+				<Input placeholder="777 Torybrook Lane" onChange={event => setAddress(event.currentTarget.value)} />
+			</FormControl>
+			<FormControl id="zip" isRequired>
+				<FormLabel>
+					Zip
+				</FormLabel>
+				<Input onChange={event => setZip(event.currentTarget.value)} />
+			</FormControl>
+			<FormControl id="date" isRequired>
+				<FormLabel>
+					Event Date and Time
+				</FormLabel>
+				<Input type="datetime-local"></Input>
 			</FormControl>
 			<FormControl id="ev-desc">
 				<FormLabel>Event Description</FormLabel>
-				<Textarea />
+				<Textarea onChange={event => setDesc(event.currentTarget.value)} />
 			</FormControl>
 			</Box>
-			<Button width={500} ml={500} mt={4} type="submit">
+			<Button width={500} ml={500} mt={4} type="submit" onClick={createEvent}>
 				Submit
 			</Button>
 		</div>
