9d1c44a9e1f17197f3b2a08e026ce87aa8ec2ea4
akjoshi2 <akjoshi2003@gmail.com>
2021-10-23 19:04:36 -0500

    Readonly fields

--- frontend/src/components/VolunteerEvents/VolunteerEvents.js
+++ frontend/src/components/VolunteerEvents/VolunteerEvents.js
@@ -1,6 +1,7 @@
 import React, { useState, useCallback } from "react";
 import { FormControl, FormLabel, Box, Heading, Button, Input, InputRightAddon, InputGroup, Textarea } from "@chakra-ui/react";
 import events from "../Calendar/events";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function Events() {
 	const [name, setName] = useState();
@@ -8,6 +9,7 @@ function Events() {
 	const [desc, setDesc] = useState();
 	const [address, setAddress] = useState();
 	const [zip, setZip] = useState();
+	const { user } = useAuth0();
 
 	const createEvent = useCallback(() => {
 
@@ -18,7 +20,19 @@ function Events() {
 				<Heading>Create Event</Heading>
 			</Box>
 			<Box my={4} textAlign="left" width={500} ml={500}>
-			<FormControl my={5} id="org-name" isRequired>
+			<FormControl isReadOnly>
+				<FormLabel>
+					Organization Name
+				</FormLabel>
+				<Input defaultValue={user.name} />	
+			</FormControl>
+			<FormControl>
+				<FormLabel>
+					Email
+				</FormLabel>
+				<Input defaultValue={user.email} />
+			</FormControl>
+			<FormControl my={5} id="ev-name" isRequired>
 				<FormLabel>
 					Event Name
 				</FormLabel>
@@ -47,7 +61,7 @@ function Events() {
 				<Textarea onChange={event => setDesc(event.currentTarget.value)} />
 			</FormControl>
 			</Box>
-			<Button width={500} ml={500} mb={5} type="submit" onClick={createEvent}>
+			<Button width={500} ml={500} mb={5} mt={4} type="submit" onClick={createEvent}>
 				Submit
 			</Button>
 		</div>
